0ca141f272b3e3c8a85efafb2bb28d2a74399649
Paul Montag <paul-montag@target.com>
2025-12-11 04:36:19 -0600

    idk some stuff

--- src/collection.rs
+++ src/collection.rs
@@ -1,4 +1,5 @@
-use crate::{CollectionConfig, EnvironmentLoader, Result, Specified};
+use crate::{CollectedTemplateGroup, CollectionConfig, EnvironmentLoader, Result, Specified};
+use anyhow::anyhow;
 use clap::ArgMatches;
 use reqwest::ClientBuilder;
 
@@ -21,27 +22,31 @@ impl<'a, E: EnvironmentLoader<Specified> + Copy> CollectionBuilder<'a, E> {
         self
     }
 
-    pub fn build<F>(mut self, _client_builder: F) -> Result<Collection>
+    pub fn build<F>(mut self, _client_builder: F) -> Result<Collection<'a, E>>
     where
         F: Fn(ClientBuilder) -> Result<ClientBuilder>,
     {
-        // TODO: you were here, just testing things out
-        let groups = self.config.unwrap().templates(self.env_loader);
-        for group in groups {
+        Ok(Collection {
+            templates: self
+                .config
+                .ok_or_else(|| anyhow!("must specify config for collection"))?
+                .templates(self.env_loader),
+        })
+    }
+}
+
+pub struct Collection<'a, E: EnvironmentLoader<Specified> + Copy> {
+    templates: CollectedTemplateGroup<'a, E>,
+}
+
+impl<'a, E: EnvironmentLoader<Specified> + Copy> Collection<'a, E> {
+    pub fn run(self, args: &ArgMatches, dry: bool) -> Result<()> {
+        dbg!(args, dry);
+        for group in self.templates {
             for template in group {
                 dbg!(template?);
             }
         }
-
-        Ok(Collection {})
-    }
-}
-
-pub struct Collection {}
-
-impl Collection {
-    pub fn run(self, args: &ArgMatches, dry: bool) -> Result<()> {
-        dbg!(args, dry);
         Ok(())
     }
 }
